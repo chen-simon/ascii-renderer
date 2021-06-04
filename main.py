6705a1a646f6120ed3155b313b638d712babac81
Simon Chen <simonchen.sc.2002@gmail.com>
2021-06-04 14:29:28 -0400

    Add test models

---
+++ main.py
@@ -0,0 +1,10 @@
+import pywavefront as wf
+
+
+def load_scene() -> None:
+    """ Loads the example scene. """
+    pass
+
+
+if __name__ == '__main__':
+    wf.Wavefront('objects/bulbasaur/bulbasaur.obj')
