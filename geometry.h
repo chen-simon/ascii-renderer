6748bf7ee71dca42f19d85168eed84f4208b6f49
Simon Chen <simonchen.sc.2002@gmail.com>
2022-01-07 17:38:59 -0500

    Add basic vector operations

--- geometry.h
+++ geometry.h
@@ -1,5 +1,8 @@
+#include <iostream>
 #include <vector>
+#include <math.h>
 
+using namespace std;
 using std::vector;
 
 /**
@@ -11,6 +14,40 @@ class Vector3 {
         float y;
         float z;
 
+        // Static vector operations
+        static Vector3 add(Vector3 v1, Vector3 v2) {
+            float x = v1.x + v2.x;
+            float y = v1.y + v2.y;
+            float z = v1.z + v2.z;
+
+            return Vector3(x, y, z); 
+        }
+
+        static Vector3 times(float scalar, Vector3 v1) {
+            float x = scalar * v1.x;
+            float y = scalar * v1.y;
+            float z = scalar * v1.z;
+
+            return Vector3(x, y, z); 
+        }
+
+        static Vector3 minus(Vector3 v1, Vector3 v2) {
+            return add(v1, times(-1, v2)); 
+        }
+
+        // Instance vector operations
+        float norm() {
+            return sqrt(x * x + y * y + z * z);
+        }
+
+        /**
+         * @brief Print the vector to console
+         */
+        void print() {
+            cout << "(" << x << ", " << y << ", "  << z << ")" << endl;
+        }
+
+        // Constructors
         Vector3 () {
             x = 0.0f;
             y = 0.0f;
@@ -38,6 +75,11 @@ class Tri {
             this->p2 = p2;
             this->p3 = p3;
         }
+
+        Vector3 norm() {
+            Vector3 d1 = Vector3::minus(p1, p2);
+            Vector3 d2 = Vector3::minus(p2, p3);
+        }
 };
 
 /**
@@ -49,5 +91,24 @@ class Object {
 
         Object(vector<Tri> tris) {
             this->tris = tris;
+        }
+};
+
+/**
+ * @brief The camera viewport
+ */
+class Camera {
+    public:
+        Vector3 position;
+        Vector3 rotation;
+        float width;
+        float height;
+    
+        Camera() {
+            // Default camera values
+            position = Vector3();
+            rotation = Vector3();
+            width = 10;
+            height = 3;
         }
 };
--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int main() {
-    Vector3 point = Vector3(1.0f, 2.0f, 3.0f);
-    cout << point.x << endl;
-    cout << point.y << endl;
-    cout << point.z;
+    Vector3 point(1.0f, 2.0f, 3.0f);
+    float norm = point.norm();
+    point.print();
+    cout << norm;
     string str;
     cin >> str;
     return 0;
