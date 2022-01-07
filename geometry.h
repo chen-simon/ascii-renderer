f70ff533ce5ddb1d95ccc8d3f92960e9006d5b3b
Simon Chen <simonchen.sc.2002@gmail.com>
2022-01-07 17:01:58 -0500

    Make geometry classes

--- geometry.h
+++ geometry.h
@@ -1,9 +1,53 @@
+#include <vector>
 
+using std::vector;
+
+/**
+ * @brief A vector in R3
+ */
+class Vector3 {
+    public:
+        float x;
+        float y;
+        float z;
+
+        Vector3 () {
+            x = 0.0f;
+            y = 0.0f;
+            z = 0.0f;
+        }
+
+        Vector3(int x, int y, int z) {
+            this->x = x;
+            this->y = y;
+            this->z = z;
+        }
+};
+
+/**
+ * @brief A triangular face in R3
+ */
+class Tri {
+    public:
+        Vector3 p1;
+        Vector3 p2;
+        Vector3 p3;
+    
+        Tri(Vector3 p1, Vector3 p2, Vector3 p3) {
+            this->p1 = p1;
+            this->p2 = p2;
+            this->p3 = p3;
+        }
+};
 
 /**
  * @brief A 3D object
  */
 class Object {
     public:
-    
-}
+        vector<Tri> tris;
+
+        Object(vector<Tri> tris) {
+            this->tris = tris;
+        }
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 #include "geometry.h"
+using namespace std;
 
 int main() {
-    Object obj = new Object();
+    Vector3 point = Vector3(1.0f, 2.0f, 3.0f);
+    cout << point.x << endl;
+    cout << point.y << endl;
+    cout << point.z;
+    string str;
+    cin >> str;
     return 0;
 }
