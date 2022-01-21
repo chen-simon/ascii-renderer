9b4cb2bfc45c06cafc1aad1a32d909b8d843251c
Simon Chen <simonchen.sc.2002@gmail.com>
2022-01-21 15:51:19 -0500

    Add working testing suite

--- tests/geometryTest.cpp
+++ tests/geometryTest.cpp
@@ -1,14 +1,13 @@
-#define CONFIG_CATCH_MAIN
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include "geometry.hpp"
+#include "../src/geometry.hpp"
 
-TEST_CASE("Vector addition")
+TEST_CASE("Vector addition", "[vector_operations]")
 {
     Vector3 v1(1, 1, 1);
     Vector3 v2(1, 2, 3);
 
-
-    Vector3 actual(2, 4, 5);
+    Vector3 actual(2, 3, 4);
     REQUIRE(v1 + v2 == actual);
 }
